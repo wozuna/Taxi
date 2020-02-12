4eed0c4b8d120accda72b3fff2347dbd8817fc3b
Waldir Ozuna <wozuna@hotmail.com>
2020-02-12 16:12:44 -0600

    Taxis Controller

---
+++ Taxi.Web/Data/Entities/TaxiEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Taxi.Web.Data.Entities
+{
+    public class TaxiEntity
+    {
+        public int Id { get; set; }
+
+        [StringLength(6, MinimumLength = 6, ErrorMessage = "The {0} field must have {1} characters.")]
+        [RegularExpression(@"^([A-Za-z]{3}\d{3})$", ErrorMessage = "The field {0} must start three characters and ends with three numbers.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string Plaque { get; set; }
+    }
+}
+
